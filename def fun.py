d52430faf1c3d71a61abb571f4724dc0e1a8950c
hudaazeem440 <156990131+hudaazeem440@users.noreply.github.com>
2024-01-22 00:45:26 -0800

    Add files via upload

---
+++ def fun.py
@@ -0,0 +1,26 @@
+def sum():
+    x=int(input("enter the first no"))
+    y=int(input("enter the second no"))
+    z=x+y
+    print("sum of two no =" , z)
+def subtract():
+    x=int(input("enter the first no"))
+    y=int(input("enter the second no"))
+    z=x-y
+    print("subtract of two no =", z)
+def multiply():
+    x=int(input("enter the first no"))
+    y=int(input("enter the second no"))
+    z=x*y
+    print("multiply of two no =", z)
+    
+def divide():
+    x=int(input("enter the first no"))
+    y=int(input("enter the second no"))
+    z=x/y
+    print("divide of two no =" , z)
+    
+sum()
+subtract()
+multiply()
+divide()
